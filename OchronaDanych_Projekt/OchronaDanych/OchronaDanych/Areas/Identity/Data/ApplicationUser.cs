710ed7a6746ae481c83df461e4c8357948f14a64
“Pawel <skok2022@gmail.com>
2022-01-23 19:41:28 +0100

    Adding project

---
+++ OchronaDanych_Projekt/OchronaDanych/OchronaDanych/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace OchronaDanych.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Nick { get; set; }
+        public string MasterPassword { get; set; }
+        public List<DomainPassword> Passwords { get; set; }
+        public bool IsLocked { get; set; }
+    }
+}
---
+++ OchronaDanych_Projekt/OchronaDanych/OchronaDanych/Areas/Identity/Data/DomainPassword.cs
@@ -0,0 +1,10 @@
+﻿namespace OchronaDanych.Areas.Identity.Data
+{
+    public class DomainPassword
+    {
+        public int ID { get; set; } 
+        public ApplicationUser User { get; set; }
+        public string Domain { get; set; }
+        public string Password { get; set; }
+    }
+}
